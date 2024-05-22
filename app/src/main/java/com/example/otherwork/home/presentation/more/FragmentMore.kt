f712f85ec258991ec94338fab4a8a596cfd0e2c3
Mamdouh <mamdouhsobhy923@gmail.com>
2024-05-22 20:44:07 +0300

    push more fragment

--- app/src/main/java/com/example/otherwork/home/presentation/more/FragmentMore.kt
+++ app/src/main/java/com/example/otherwork/home/presentation/more/FragmentMore.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import com.example.otherwork.R
 import com.example.otherwork.databinding.FragmentMoreBinding
 
 class FragmentMore : Fragment() {
@@ -22,6 +25,36 @@ class FragmentMore : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        addListenerOnView()
+    }
+
+    private fun addListenerOnView() {
+        binding.layoutChangeLanguage.setOnClickListener {
+            showLanguageDialog()
+        }
+    }
+
+    private fun showLanguageDialog() {
+        val languages = arrayOf(
+            getString(R.string.english),
+            getString(R.string.arabic),
+            getString(R.string.cancel)
+        )
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, languages)
+
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.change_language))
+            .setAdapter(adapter) { dialog, which ->
+                val selectedLanguage = languages[which]
+                if (selectedLanguage == getString(R.string.cancel)) {
+                    dialog.dismiss()
+                } else {
+                    //set local language
+                    dialog.dismiss()
+                }
+            }
+            .setCancelable(false)
+            .show()
     }
 
 }
